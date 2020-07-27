0767238abb599aaf5daec92218fb7877f2629584
joenapper <joe.napper@hotmail.co.uk>
2020-07-27 15:31:42 +0000

    Links slide in when hamburger icon is toggled

--- static/js/header.js
+++ static/js/header.js
@@ -1,16 +1,28 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
-const header = document.querySelector('header');
 
 // Toggle Nav
 function navSlide() {
     burger.addEventListener('click', () => {
         // Overlay Slide In
         nav.classList.toggle('nav-active');
+        // Links Slide In
+        linkSlide();
         // Burger Animation
         burger.classList.toggle('burger-toggle');
     });
 };
+
+// Links Slide In
+function linkSlide() {
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 1.0s ease forwards ${index / 5 + 0.4}s`;
+        };
+    });
+}; 
 
 navSlide()
