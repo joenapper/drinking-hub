4370f5dfb0278633a75cdca69dbb3f79c5155602
joenapper <joe.napper@hotmail.co.uk>
2020-07-27 15:26:38 +0000

    Added hamburger animation on click

--- static/js/header.js
+++ static/js/header.js
@@ -1,11 +1,15 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+const header = document.querySelector('header');
 
 // Toggle Nav
 function navSlide() {
     burger.addEventListener('click', () => {
         // Overlay Slide In
         nav.classList.toggle('nav-active');
+        // Burger Animation
+        burger.classList.toggle('burger-toggle');
     });
 };
 
