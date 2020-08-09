8a7a6549e3f49572db724e9aaa7b0d535915ff2c
joenapper <joe.napper@hotmail.co.uk>
2020-08-09 12:46:59 +0000

    Added comments to help improve code readability in javaScript file

--- static/js/main.js
+++ static/js/main.js
@@ -27,6 +27,8 @@ function linkSlide() {
 
 navSlide()
 
+
+// Delete Validation Pop up
 const modal = document.getElementById('modalPopup');
 const popUp = document.getElementById('delValidation');
 const closeModalBtn = document.getElementsByClassName('closeModalBtn')[0];
@@ -37,14 +39,15 @@ closeModalBtn.addEventListener('click', closeModal);
 noValidation.addEventListener('click', closeModal);
 window.addEventListener('click', clickOutside);
 
+// When the delete button is clicked the pop up will appear
 function openModal() {
     modal.style.display = 'block';
 }
-
+// When either the 'No' or 'X' buttons are clicked, the pop up will close 
 function closeModal() {
     modal.style.display = 'none'
 }
-
+// If the user clicks anywhere but the popup, it will automatically close
 function clickOutside(e) {
     if(e.target == modal) {
         modal.style.display = 'none'
