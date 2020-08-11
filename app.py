e6492b5df9d365642aa00d90313a9875054d72f0
joenapper <joe.napper@hotmail.co.uk>
2020-08-11 14:53:00 +0000

    Debug set to False

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def privacy_policy():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
